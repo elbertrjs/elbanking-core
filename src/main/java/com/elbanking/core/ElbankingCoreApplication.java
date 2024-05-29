deb1967b5e8e2c2ff07df61f9a0a4ace7d6bc267
Elbert Reynard <elbertrjs@gmail.com>
2024-05-30 00:48:51 +0700

    Initial project

---
+++ src/main/java/com/elbanking/core/ElbankingCoreApplication.java
@@ -0,0 +1,13 @@
+package com.elbanking.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElbankingCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElbankingCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elbanking/core/ElbankingCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elbanking.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElbankingCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
