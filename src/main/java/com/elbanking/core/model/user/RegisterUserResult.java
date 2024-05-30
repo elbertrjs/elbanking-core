5907b55dc91502f07426b95a2f0134d7f7a2e495
Elbert Reynard <elbertrjs@gmail.com>
2024-05-31 01:54:12 +0700

    Remove AllArgsConstructor from RegisterUserResult to prevent constructor being declared twice

--- src/main/java/com/elbanking/core/model/user/RegisterUserResult.java
+++ src/main/java/com/elbanking/core/model/user/RegisterUserResult.java
@@ -1,12 +1,10 @@
 package com.elbanking.core.model.user;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class RegisterUserResult {
