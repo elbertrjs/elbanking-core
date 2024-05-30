1fdd58c61a46a979f83d996e7b955eb038cafe75
Elbert Reynard <elbertrjs@gmail.com>
2024-05-31 02:02:19 +0700

    Add dependency injection for mapper

--- src/main/java/com/elbanking/core/mapper/UserMapper.java
+++ src/main/java/com/elbanking/core/mapper/UserMapper.java
@@ -4,7 +4,7 @@ import com.elbanking.core.model.user.RegisterUserRequest;
 import com.elbanking.core.model.user.SignUpRequestForm;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface UserMapper {
     RegisterUserRequest convertToRegisterUserRequest(SignUpRequestForm signUpRequestForm);
 }
