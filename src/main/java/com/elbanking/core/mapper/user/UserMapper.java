9d59865667a861ce5de744f2aeb7bfeaeaa7e2a1
Elbert Reynard <elbertrjs@gmail.com>
2024-05-31 13:20:41 +0700

    Add UserManageServiceImpl insertUser repository implementation

--- src/main/java/com/elbanking/core/mapper/user/UserMapper.java
+++ src/main/java/com/elbanking/core/mapper/user/UserMapper.java
@@ -2,9 +2,15 @@ package com.elbanking.core.mapper.user;
 
 import com.elbanking.core.model.user.RegisterUserRequest;
 import com.elbanking.core.model.user.SignUpRequestForm;
+import com.elbanking.core.model.user.UserDAO;
+import com.elbanking.core.model.user.UserDO;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     RegisterUserRequest convertToRegisterUserRequest(SignUpRequestForm signUpRequestForm);
+
+    UserDO convertToUserDO(UserDAO userDAO);
+
+    UserDAO convertToUserDAO(UserDO userDo);
 }
--- src/main/java/com/elbanking/core/service/user/UserManageServiceImpl.java
+++ src/main/java/com/elbanking/core/service/user/UserManageServiceImpl.java
@@ -1,11 +1,22 @@
 package com.elbanking.core.service.user;
 
+import com.elbanking.core.mapper.user.UserMapper;
 import com.elbanking.core.model.user.UserDAO;
+import com.elbanking.core.model.user.UserDO;
+import com.elbanking.core.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserManageServiceImpl implements UserManageService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserMapper userMapper;
     @Override
     public UserDAO insertUser(UserDAO userDAO) {
-        // call repository here
-        return userDAO;
+        UserDO userDO = userMapper.convertToUserDO(userDAO);
+        UserDO createdUserDO = (UserDO) userRepository.save(userDO);
+        return userMapper.convertToUserDAO(createdUserDO);
     }
 }
