c14746a03b15099d9ac053d32b82e602c1707ad3
Elbert Reynard <elbertrjs@gmail.com>
2024-05-31 13:14:47 +0700

    Add UserDO lombok annotation

--- src/main/java/com/elbanking/core/model/user/UserDO.java
+++ src/main/java/com/elbanking/core/model/user/UserDO.java
@@ -2,8 +2,16 @@ package com.elbanking.core.model.user;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserDO {
     @Id
     private String id;
