1bcb8e3c07da802c52e44b9cb2615feeba5d58e0
Elbert Reynard <elbertrjs@gmail.com>
2024-06-01 23:06:18 +0700

    Add entity scan

--- src/main/java/com/elbanking/core/application/ElbankingCoreApplication.java
+++ src/main/java/com/elbanking/core/application/ElbankingCoreApplication.java
@@ -2,10 +2,12 @@ package com.elbanking.core.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.elbanking.core")
-@EnableJpaRepositories("com.elbanking.core")
+@EnableJpaRepositories(basePackages = "com.elbanking.core")
+@EntityScan(basePackages ="com.elbanking.core")
 public class ElbankingCoreApplication {
 
     public static void main(String[] args) {
