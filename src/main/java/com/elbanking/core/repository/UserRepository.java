250b83a6a4fbab360164baf31b09af1972ea853b
Elbert Reynard <elbertrjs@gmail.com>
2024-06-01 23:07:08 +0700

    Add UserDO usage on UserRepository

--- src/main/java/com/elbanking/core/repository/UserRepository.java
+++ src/main/java/com/elbanking/core/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package com.elbanking.core.repository;
 
+import com.elbanking.core.model.user.UserDO;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository<UserDO,ID> extends CrudRepository {
+public interface UserRepository extends CrudRepository<UserDO,String> {
 
 }
