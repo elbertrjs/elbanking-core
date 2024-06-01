9863c7a43e38bde5a3a48ea6b8d259367e125620
Elbert Reynard <elbertrjs@gmail.com>
2024-06-01 23:53:57 +0700

    Add GeneratedValue annotation for id

--- src/main/java/com/elbanking/core/model/user/UserDO.java
+++ src/main/java/com/elbanking/core/model/user/UserDO.java
@@ -1,6 +1,8 @@
 package com.elbanking.core.model.user;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -16,6 +18,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "users")
 public class UserDO {
     @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private String id;
     private String email;
     private String password;
