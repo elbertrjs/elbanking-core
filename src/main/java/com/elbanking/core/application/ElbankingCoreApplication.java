d0af209823c4b57534b037f80e9bfb9a8806d621
Elbert Reynard <elbertrjs@gmail.com>
2024-06-01 22:10:48 +0700

    Add EnableJpaRepositories annotation

--- src/main/java/com/elbanking/core/application/ElbankingCoreApplication.java
+++ src/main/java/com/elbanking/core/application/ElbankingCoreApplication.java
@@ -2,8 +2,10 @@ package com.elbanking.core.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.elbanking.core")
+@EnableJpaRepositories("com.elbanking.core")
 public class ElbankingCoreApplication {
 
     public static void main(String[] args) {
