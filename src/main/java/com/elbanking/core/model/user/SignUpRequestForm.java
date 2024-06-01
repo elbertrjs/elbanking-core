7081a428895d99a4abe263be862707290edebb07
Elbert Reynard <elbertrjs@gmail.com>
2024-06-01 23:13:31 +0700

    Add SignUpRequestForm lombok annotations

--- src/main/java/com/elbanking/core/model/user/SignUpRequestForm.java
+++ src/main/java/com/elbanking/core/model/user/SignUpRequestForm.java
@@ -1,5 +1,14 @@
 package com.elbanking.core.model.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SignUpRequestForm {
 
     private String email;
