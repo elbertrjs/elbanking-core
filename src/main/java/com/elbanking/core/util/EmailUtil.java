0ef3c3b9eb6aae2e74438835b20eaaf6009139d6
Elbert Reynard <elbertrjs@gmail.com>
2024-06-03 14:21:21 +0700

    Modify email regex to prevent missing '.'

--- src/main/java/com/elbanking/core/util/EmailUtil.java
+++ src/main/java/com/elbanking/core/util/EmailUtil.java
@@ -4,7 +4,8 @@ import java.util.regex.Pattern;
 
 public class EmailUtil {
     public static boolean isEmailValid(String email){
-        final Pattern EMAIL_REGEX = Pattern.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", Pattern.CASE_INSENSITIVE);
+        final Pattern EMAIL_REGEX = Pattern.compile("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+                + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$", Pattern.CASE_INSENSITIVE);
         return EMAIL_REGEX.matcher(email).matches();
     }
 }
