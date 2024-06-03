33a34d19e9870a7692020dad36a74b88039f0aca
Elbert Reynard <elbertrjs@gmail.com>
2024-06-03 12:27:03 +0700

    Add signup result

--- src/main/java/com/elbanking/core/controller/user/UserController.java
+++ src/main/java/com/elbanking/core/controller/user/UserController.java
@@ -2,12 +2,13 @@ package com.elbanking.core.controller.user;
 
 import com.elbanking.core.manager.user.UserManager;
 import com.elbanking.core.mapper.user.UserMapper;
+import com.elbanking.core.model.HTTPResult;
 import com.elbanking.core.model.user.RegisterUserRequest;
 import com.elbanking.core.model.user.RegisterUserResult;
 import com.elbanking.core.model.user.SignUpRequestForm;
+import com.elbanking.core.model.user.SignUpResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,15 +21,21 @@ public class UserController {
 
     @Autowired
     private UserMapper userMapper;
+
     @PostMapping("/users")
-    public ResponseEntity<?> signUp(@RequestBody SignUpRequestForm signUpRequestForm){
+    public HTTPResult signUp(@RequestBody SignUpRequestForm signUpRequestForm){
+
 
         RegisterUserRequest registerUserRequest = userMapper.convertToRegisterUserRequest(signUpRequestForm);
         RegisterUserResult registerUserResult = userManager.registerUser(registerUserRequest);
 
-        return
-            ResponseEntity
-                .status(HttpStatus.OK)
-                .body(registerUserResult);
+
+        SignUpResult signUpResult = SignUpResult.builder()
+                .status(HttpStatus.OK.value())
+                .message("Success")
+                .data(registerUserResult)
+                .build();
+
+        return signUpResult;
     }
 }
