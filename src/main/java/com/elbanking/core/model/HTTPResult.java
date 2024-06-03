72667ee5cec46055130274f071c42cb31ecd8151
Elbert Reynard <elbertrjs@gmail.com>
2024-06-03 10:21:35 +0700

    Add messasge and data to HTTPResult

--- src/main/java/com/elbanking/core/model/HTTPResult.java
+++ src/main/java/com/elbanking/core/model/HTTPResult.java
@@ -4,11 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
-public class HTTPResult {
-    private String status;
+@SuperBuilder
+public abstract class HTTPResult {
+    private Integer status;
+    private String message;
+    private ResultData data;
 }
