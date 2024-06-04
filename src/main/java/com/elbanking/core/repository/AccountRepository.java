661f9c6b0f7db26b979d29498440dd364f568d18
Elbert Reynard <elbertrjs@gmail.com>
2024-06-04 16:24:01 +0700

    Modify AccountRepository findByUserId return type

--- src/main/java/com/elbanking/core/repository/AccountRepository.java
+++ src/main/java/com/elbanking/core/repository/AccountRepository.java
@@ -1,6 +1,7 @@
 package com.elbanking.core.repository;
 
 
+import com.elbanking.core.model.account.AccountDAO;
 import com.elbanking.core.model.account.AccountDO;
 import com.elbanking.core.model.user.UserDO;
 import org.springframework.data.jpa.repository.Query;
@@ -10,5 +11,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AccountRepository extends CrudRepository<AccountDO,String> {
     @Query(value = "SELECT * FROM accounts WHERE user_id = ?1", nativeQuery = true)
-    UserDO findByUserId(String userId);
+    AccountDO findByUserId(String userId);
 }
