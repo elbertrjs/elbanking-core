0f924d2f5cb108eff241df9ffd6a533e4580f015
Elbert Reynard <elbertrjs@gmail.com>
2024-06-04 13:21:41 +0700

    Add insertAccount method

--- src/main/java/com/elbanking/core/service/account/AccountManageService.java
+++ src/main/java/com/elbanking/core/service/account/AccountManageService.java
@@ -3,7 +3,7 @@ package com.elbanking.core.service.account;
 import com.elbanking.core.model.account.AccountDAO;
 
 public interface AccountManageService {
-    public AccountDAO createAccount();
+    public AccountDAO insertAccount(AccountDAO accountDAO);
 
     public AccountDAO queryAccount(String id);
 }
--- src/main/java/com/elbanking/core/service/account/AccountManageServiceImpl.java
+++ src/main/java/com/elbanking/core/service/account/AccountManageServiceImpl.java
@@ -1,13 +1,25 @@
 package com.elbanking.core.service.account;
 
+import com.elbanking.core.mapper.account.AccountMapper;
 import com.elbanking.core.model.account.AccountDAO;
+import com.elbanking.core.model.account.AccountDO;
+import com.elbanking.core.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountManageServiceImpl implements AccountManageService {
+    @Autowired
+    private AccountMapper accountMapper;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
     @Override
-    public AccountDAO createAccount() {
-        return null;
+    public AccountDAO insertAccount(AccountDAO accountDAO) {
+        AccountDO userDO = accountMapper.convertToAccountDO(accountDAO);
+        AccountDO accountDO = (AccountDO) accountRepository.save(userDO);
+        return accountMapper.convertToAccountDAO(accountDO);
     }
 
     @Override
