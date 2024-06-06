f51ff4ece4df88cd65c2568b10e55499909dc00b
Elbert Reynard <elbertrjs@gmail.com>
2024-06-07 01:35:23 +0700

    Add test profile

--- src/test/java/com/elbanking/core/application/ElbankingCoreApplicationTests.java
+++ src/test/java/com/elbanking/core/application/ElbankingCoreApplicationTests.java
@@ -2,8 +2,10 @@ package com.elbanking.core.application;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles
 class ElbankingCoreApplicationTests {
 
     @Test
