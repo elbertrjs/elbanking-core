26e4c30179d71539dfaee49ec6a73e5f15807931
Elbert Reynard <elbertrjs@gmail.com>
2024-06-06 23:29:49 +0700

    Modify accountMapper test case

--- src/test/java/com/elbanking/core/mapper/AccountMapperTest.java
+++ src/test/java/com/elbanking/core/mapper/AccountMapperTest.java
@@ -1,10 +1,12 @@
 package com.elbanking.core.mapper;
 
+import com.elbanking.core.constant.CurrencyCodeConstant;
 import com.elbanking.core.constant.account.AccountConstant;
 import com.elbanking.core.mapper.account.AccountMapper;
 import com.elbanking.core.mapper.account.AccountMapperImpl;
 import com.elbanking.core.model.account.AccountDAO;
 import com.elbanking.core.model.account.AccountDO;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,32 +27,53 @@ public class AccountMapperTest {
 
     @Test
     public void testConvertToAccountDO(){
-           AccountDAO accountDAO = AccountDAO
-                   .builder()
-                   .accountId(UUID.randomUUID().toString())
-                   .userId(UUID.randomUUID().toString())
-                   .balanceValue(50000000L)
-                   .gmtCreate(new Date())
-                   .gmtModified(new Date())
-                   .build();
+        String accountId = "80304ceb-b613-4748-ad87-8f6300e22764";
+        String userId = "88d38f29-857f-4400-a643-8e2aacf3b5b0";
+        Long balanceValue = 50000000L;
+        String balanceCurrency = CurrencyCodeConstant.IDR;
+        Date currentDate = new Date();
+        AccountDAO accountDAO = AccountDAO
+                .builder()
+                .accountId(accountId)
+                .userId(userId)
+                .balanceValue(balanceValue)
+                .balanceCurrency(balanceCurrency)
+                .gmtCreate(currentDate)
+                .gmtModified(currentDate)
+                .build();
 
-            AccountDO accountDO = accountMapper.convertToAccountDO(accountDAO);
-            System.out.println(accountDO.toString());
+        AccountDO accountDO = accountMapper.convertToAccountDO(accountDAO);
+        assertEquals(accountDAO,accountDO);
     }
 
     @Test
     public void testConvertToAccountDAO(){
+        String accountId = "80304ceb-b613-4748-ad87-8f6300e22764";
+        String userId = "88d38f29-857f-4400-a643-8e2aacf3b5b0";
+        Long balanceValue = 50000000L;
+        String balanceCurrency = CurrencyCodeConstant.IDR;
+        Date currentDate = new Date();
+
         AccountDO accountDO = AccountDO
                 .builder()
-                .accountId(UUID.randomUUID())
-                .userId(UUID.randomUUID())
-                .balanceCurrency(AccountConstant.DEFAULT_CURRENCY)
-                .balanceValue(12312L)
-                .gmtCreate(new Date())
-                .gmtModified(new Date())
+                .accountId(UUID.fromString(accountId))
+                .userId(UUID.fromString(userId))
+                .balanceValue(balanceValue)
+                .balanceCurrency(balanceCurrency)
+                .gmtCreate(currentDate)
+                .gmtModified(currentDate)
                 .build();
 
         AccountDAO accountDAO = accountMapper.convertToAccountDAO(accountDO);
-        System.out.println(accountDAO.toString());
+        assertEquals(accountDAO,accountDO);
+    }
+
+    private void assertEquals(AccountDAO accountDAO, AccountDO accountDO){
+        Assert.assertEquals(accountDAO.getAccountId(),accountDO.getAccountId().toString());
+        Assert.assertEquals(accountDAO.getUserId(),accountDO.getUserId().toString());
+        Assert.assertEquals(accountDAO.getBalanceValue(),accountDO.getBalanceValue());
+        Assert.assertEquals(accountDAO.getBalanceCurrency(),accountDO.getBalanceCurrency());
+        Assert.assertEquals(accountDAO.getGmtCreate(),accountDO.getGmtCreate());
+        Assert.assertEquals(accountDAO.getGmtModified(),accountDO.getGmtModified());
     }
 }
