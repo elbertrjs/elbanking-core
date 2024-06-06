79974c8f6a556b68a8c00902c9192260e41c7e13
Elbert Reynard <elbertrjs@gmail.com>
2024-06-06 18:45:30 +0700

    Move AUTH_SECRET_KEY to application.properties

--- src/main/java/com/elbanking/core/service/authentication/JwtService.java
+++ src/main/java/com/elbanking/core/service/authentication/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -17,7 +18,12 @@ import java.util.function.Function;
 @Component
 public class JwtService {
 
-    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+    public final String AUTH_SECRET_KEY;
+
+    public JwtService(@Value("${elbanking.core.secret}") String secret){
+        this.AUTH_SECRET_KEY = secret;
+    }
+
     public String generateToken(String userName) {
         Map<String, Object> claims = new HashMap<>();
         return createToken(claims, userName);
@@ -33,7 +39,7 @@ public class JwtService {
     }
 
     private Key getSignKey() {
-        byte[] keyBytes= Decoders.BASE64.decode(SECRET);
+        byte[] keyBytes= Decoders.BASE64.decode(AUTH_SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
